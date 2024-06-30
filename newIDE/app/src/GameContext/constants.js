97107f80e77036e427cd2b837ed8ded1f4f6f7fa
vijay kiran samrath <vijaysamrath462@gmail.com>
2024-06-30 19:29:37 +0530

    Updated codes

---
+++ newIDE/app/src/GameContext/constants.js
@@ -0,0 +1,4 @@
+import GameMarketplace from './GameMarketplace.json';
+
+export const GameMarketAddress = '0xeDc2818C8Ed140158d17e576121f54CfA2371eC1';
+export const GameMarketAddressABI = GameMarketplace.abi;
---
+++ newIDE/app/src/context/constants.js
@@ -0,0 +1,7 @@
+import NFTMarketplace from './NFTMarketplace.json';
+
+export const MarketAddress = '0xB29e037d52ebdf6b7E6aba49dB9C983082075989';
+export const MarketAddressABI = NFTMarketplace.abi;
+
+
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3 localhost address 
